bfea11bdc630164beff510906d941c8f3f896dd5
TanviBurji <118246614+TanviBurji@users.noreply.github.com>
2025-07-16 11:49:58 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,10 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "AI & ML Engineer",
+        
+          
+          
         ],
         autoStart: true,
         loop: true,
