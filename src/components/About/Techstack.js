a7013eced21e8db7c52b95d1296a5aba1b4c43e1
TanviBurji <118246614+TanviBurji@users.noreply.github.com>
2025-07-16 11:57:23 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -16,6 +16,10 @@ import {
   SiNextdotjs,
   SiSolidity,
   SiPostgresql,
+  SiHtml5,
+  SiCss3,
+  SiC,
+  SiMysql,
 } from "react-icons/si";
 import { TbBrandGolang } from "react-icons/tb";
 
@@ -26,28 +30,45 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      
-      
-      
+
       <Col xs={4} md={2} className="tech-icons">
         <SiSolidity />
       </Col>
-     
-      
+
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
-      
-      
+
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col>
+
+      {/* 🔽 Newly added tech icons below */}
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCss3 />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiC />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+
     </Row>
   );
 }
